da7cb160e5e56259a59567463128102fa1f154d5
Susi <Susanne.Malheiros@online.de>
2019-12-11 12:58:11 +0100

    added output

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,18 @@ import { GitHub } from '@actions/github'
 import octokit from './octokit'
 
 
-let regexp: RegExp = /\d*\.?\d*\s?([C,c][C,c]2|[C,c]oder [C,c]oins?)/;
+let regexpCC2: RegExp = /\d*\.?\d*\s?([C,c][C,c]2|[C,c]oder [C,c]oins?)/;
 
 async function run() {
   try {
-    console.log("action  started  ")
-/*    const message = core.getInput('message')
-    await octokit.issues.createComment({
+    console.log("context is " + JSON.stringify(context, null, 2))
+    const message = core.getInput('message')
+    // if (regexpCC2.test(message))
+    // {
+      
+    // }
+
+/*    await octokit.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
       issue_number: context.issue.number,
