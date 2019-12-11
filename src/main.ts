cba156107f286b0704075b742d5eba89cfb96c17
Susi <Susanne.Malheiros@online.de>
2019-12-11 10:48:52 +0100

    Added regex for finding CC2

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,19 @@ import { GitHub } from '@actions/github'
 import octokit from './octokit'
 
 
-
+let regexp: RegExp = /\d*\.?\d*\s?([C,c][C,c]2|[C,c]oder [C,c]oins?)/;
 
 async function run() {
   try {
     console.log("action  started  ")
-    const message = core.getInput('message')
+/*    const message = core.getInput('message')
     await octokit.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
       issue_number: context.issue.number,
       body: message
     })
-
+*/
   } catch (error) {
     core.setFailed(error.message)
   }
