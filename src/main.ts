4150625c9e3baf51699e41b20778320a5dff2ccb
Susi <Susanne.Malheiros@online.de>
2019-12-11 14:18:16 +0100

    Entire body again

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,19 @@ let regexpCC2: RegExp = /\d*\.?\d*\s?([C,c][C,c]2|[C,c]oder [C,c]oins?)/;
 
 async function run() {
   try {
-    //console.log("context is " + JSON.stringify(context, null, 2))
+    console.log("context is " + JSON.stringify(context, null, 2))
     const message = core.getInput('message')
     const prDescription = context.payload.pull_request!.comment.body
-    console.log(JSON.stringify(prDescription, null,2))
-    // if (regexpCC2.test(message))
-    // {
-      
-    // }
-
+    const description = JSON.stringify(prDescription, null, 2)
+    console.log("This is the message I found: " + JSON.stringify(prDescription, null,2))
+    /*if (regexpCC2.test(description))
+    {
+      console.log("It it as an amount!")
+    }else
+    {
+      console.log("It does NOT have an amount")
+    }
+*/
 /*    await octokit.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
