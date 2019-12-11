a09a940269070aa1a15518c5ce1369cde1a1b267
Susi <Susanne.Malheiros@online.de>
2019-12-11 15:55:19 +0100

    Continuing the search for the description

--- src/main.ts
+++ src/main.ts
@@ -12,27 +12,19 @@ async function run() {
     //NOTE: check for Null!
     //console.log("context is " + JSON.stringify(context, null, 2))
     const message = core.getInput('message')
-    //const prDescription = context.payload!.base.body
+    const prDescription = context.payload
+    console.log(JSON.stringify(prDescription, null, 2))
     const description = "Hello, if you solve this issue, you can get 220.9283 CC2."
     console.log("This is the message I found: " + description)
     if (regexpCC2.test(description))
-    {
+    { // Note: If user specifies more than one amount ("e.g 'I could give you 100 CC2 or 220 CC2, the following
+      // function chooses the first one (i.e. 100 CC2) automatically.)
       const amountPlusCC2 = regexpCC2.exec(description)![0]
       // We only want the amount, so let's cut the 'CC2' from the string
-      console.log("It has an amount!"+ amountPlusCC2.substring(-4, amountPlusCC2.length-4))
-      console.log("We found these many expressions: ", regexpCC2.exec(description)!.length)
+      console.log("The pull request definition specifies the following amount!"+ amountPlusCC2.substring(-4, amountPlusCC2.length-4))
     }else
     {
-      console.log("It does NOT have an amount")
-    }
-    const description1 = "Hello, if you solve this issue, you can get 220.9283 CC2 or 100 CC2 or 100000 CC2. You choose:)"
-    console.log("This is the message I found: " + description1)
-    if (regexpCC2.test(description1))
-    {
-      console.log("We found these many expressions: ", regexpCC2.exec(description1)!)
-    }else
-    {
-      console.log("It does NOT have an amount")
+      console.log("The pull request definition does NOT have an amount")
     }
   } catch (error) {
     core.setFailed(error.message)
