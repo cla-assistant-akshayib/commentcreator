83785bb9702aba7998dfcabfd46f4272421a6e57
Susi <Susanne.Malheiros@online.de>
2019-12-11 14:40:48 +0100

    Starting to work on the logic

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,30 @@ import { GitHub } from '@actions/github'
 import octokit from './octokit'
 
 
-let regexpCC2: RegExp = /\d*\.?\d*\s?([C,c][C,c]2|[C,c]oder [C,c]oins?)/;
+let regexpCC2: RegExp = /\d\d*?\.?\d*\s?[C,c][C,c]2/;
 
 async function run() {
   try {
-    console.log("context is " + JSON.stringify(context, null, 2))
+    //console.log("context is " + JSON.stringify(context, null, 2))
     const message = core.getInput('message')
-    const prDescription = context.payload!.base.body
-    const description = JSON.stringify(prDescription, null, 2)
-    console.log("This is the message I found: " + JSON.stringify(prDescription, null,2))
-    /*if (regexpCC2.test(description))
+    //const prDescription = context.payload!.base.body
+    const description = "Hello, if you solve this issue, you can get 220 CCN."
+    console.log("This is the message I found: " + description)
+    if (regexpCC2.test(description))
     {
-      console.log("It it as an amount!")
+      console.log("It has an amount!")
     }else
     {
       console.log("It does NOT have an amount")
     }
-*/
+    const noAmountMessage = "Hello, if you want to solve this issue, I will give you cookies CCN:)"
+    if (regexpCC2.test(noAmountMessage))
+    {
+      console.log("2:It has an amount!")
+    }else
+    {
+      console.log("2:It does NOT have an amount")
+    }
 /*    await octokit.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
